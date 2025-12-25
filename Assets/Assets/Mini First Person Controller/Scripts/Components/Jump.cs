80800112a79c227c51579dbe74a955017160d5e3
Gleb <holy12578@mail.ru>
2025-12-25 17:34:29 +0300

    terrain

--- Assets/Assets/Mini First Person Controller/Scripts/Components/Jump.cs
+++ Assets/Assets/Mini First Person Controller/Scripts/Components/Jump.cs
@@ -2,33 +2,32 @@
 
 public class Jump : MonoBehaviour
 {
-    Rigidbody rigidbody;
-    public float jumpStrength = 2;
-    public event System.Action Jumped;
+	Rigidbody rigidbody;
+	public float jumpStrength = 2;
+	public event System.Action Jumped;
 
-    [SerializeField, Tooltip("Prevents jumping when the transform is in mid-air.")]
-    GroundCheck groundCheck;
+	[SerializeField, Tooltip("Prevents jumping when the transform is in mid-air.")]
+	GroundCheck groundCheck;
+	
+	private void Reset()
+	{
+		// Try to get groundCheck.
+		groundCheck = GetComponentInChildren<GroundCheck>();
+	}
 
+	private void Awake()
+	{
+		// Get rigidbody.
+		rigidbody = GetComponent<Rigidbody>();
+	}
 
-    void Reset()
-    {
-        // Try to get groundCheck.
-        groundCheck = GetComponentInChildren<GroundCheck>();
-    }
-
-    void Awake()
-    {
-        // Get rigidbody.
-        rigidbody = GetComponent<Rigidbody>();
-    }
-
-    void LateUpdate()
-    {
-        // Jump when the Jump button is pressed and we are on the ground.
-        if (Input.GetButtonDown("Jump") && (!groundCheck || groundCheck.isGrounded))
-        {
-            rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
-            Jumped?.Invoke();
-        }
-    }
+	private void LateUpdate()
+	{
+		// Jump when the Jump button is pressed and we are on the ground.
+		if (Input.GetButtonDown("Jump") && (!groundCheck || groundCheck.isGrounded))
+		{
+			rigidbody.AddForce(Vector3.up * 100 * jumpStrength);
+			Jumped?.Invoke();
+		}
+	}
 }
