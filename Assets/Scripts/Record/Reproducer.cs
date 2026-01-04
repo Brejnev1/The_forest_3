33d81a7dc1603c623a6fa4fa40343190e1696656
Gleb <holy12578@mail.ru>
2026-01-04 19:47:39 +0300

    records

--- Assets/Scripts/Record/Reproducer.cs
+++ Assets/Scripts/Record/Reproducer.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Reproducer : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro nameSong;
-    [SerializeField] private TextMeshPro timeSong;
+    [SerializeField] private TextMeshProUGUI nameSong;
+    [SerializeField] private TextMeshProUGUI creator;
+
+    [SerializeField] private Slider timeSong;
+
     private AudioSource audioSource;
     public AudioClip[] musics;
+
     public int Rand;
     public int Index;
+
+    private bool isPlaying;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        isPlaying = false;
     }
 
     public void PreviousSong()
@@ -39,9 +47,15 @@ public class Reproducer : MonoBehaviour
 
     public void PlayAudio()
     {
-        if (audioSource.clip != null) 
+        if (audioSource.clip != null && isPlaying == false)
         {
             audioSource.Play();
+            isPlaying = true;
+        }
+        else if (isPlaying == true)
+        {
+            audioSource.Pause();
+            isPlaying = false;
         }
         else
         {
@@ -50,10 +64,27 @@ public class Reproducer : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (isPlaying == true)
+        {
+            timeSong.value += Time.deltaTime;
+        }
+        if (timeSong.value == timeSong.maxValue)
+        {
+            NextSong();
+        }
+    }
+
     private void AudioSoursePlay()
     {
-        nameSong.text = audioSource.clip.nameSong;
+        isPlaying = true;
         audioSource.clip = musics[Index];
         audioSource.Play();
+        string[] informationSong = audioSource.clip.name.Split('-');
+        nameSong.text = informationSong[1];
+        creator.text = informationSong[0];
+        timeSong.maxValue = audioSource.clip.length;
+        timeSong.value = 0;
     } 
 }
