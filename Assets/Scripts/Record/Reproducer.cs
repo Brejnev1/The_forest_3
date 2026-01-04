f32a91c125490cab4f25290642f6ccc3b638f298
Gleb <holy12578@mail.ru>
2026-01-04 13:23:55 +0300

    song

--- Assets/Scripts/Record/Reproducer.cs
+++ Assets/Scripts/Record/Reproducer.cs
@@ -1,11 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Reproducer : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] private TextMeshPro nameSong;
+    [SerializeField] private TextMeshPro timeSong;
+    private AudioSource audioSource;
+    public AudioClip[] musics;
+    public int Rand;
+    public int Index;
+
+    private void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
+
+    public void PreviousSong()
+    {
+        Index--;
+        if (Index < 0)
+        { 
+            Index = 0;
+        }
+        AudioSoursePlay();
+    }
+
+    public void NextSong()
+    {
+        Index++;
+        if (Index > musics.Length - 1)
+        {
+            Index = musics.Length - 1;
+        }
+        AudioSoursePlay();
+    }
+
+    public void PlayAudio()
+    {
+        if (audioSource.clip != null) 
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            Index = 0;
+            AudioSoursePlay();
+        }
+    }
+
+    private void AudioSoursePlay()
+    {
+        nameSong.text = audioSource.clip.nameSong;
+        audioSource.clip = musics[Index];
+        audioSource.Play();
+    } 
 }
